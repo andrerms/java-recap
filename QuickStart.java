c9f5194c2f129e09a34dafbb1cf7f7448debeef7
Andre Ramos M. Souza <andrerms@hotmail.com>
2020-02-08 16:58:00 +0100

    Plain around :))

--- QuickStart.java
+++ QuickStart.java
@@ -1,5 +1,13 @@
 class QuickStart{
     public static void main(String[] args){
-        System.out.println("Here I go again!");
+        // System.out.println("Here I go again!");
+        int count = 10;
+        for(int i=0; i<count; i++){
+            String str = "";
+            for(int j=0; j<i+1; j++){
+                str = str + "*";
+            }
+            System.out.println(str);
+        }
     }
 }
